3c42f22a743c487ed03648cd1b1542e7b2ccd372
wangguangyou <15968848474@100fen.cn>
2022-06-27 18:10:36 +0800

    docs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GeistProvider, CssBaseline } from '@geist-ui/core'
-
+// eslint-disable-next-line no-undef
+window.SharedArrayBuffer = window.ArrayBuffer;
 ReactDOM.render(
   <React.StrictMode>
     <GeistProvider>
