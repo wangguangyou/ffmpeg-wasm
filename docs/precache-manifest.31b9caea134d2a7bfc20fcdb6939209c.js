9d2cab08e529fa094b876bfa2093723205d9271b
wangguangyou <15968848474@100fen.cn>
2022-06-27 17:37:01 +0800

    docs

---
+++ docs/precache-manifest.31b9caea134d2a7bfc20fcdb6939209c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8068b4ad09a3d9306ca9399618a0e19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca8cca3de527486acb21",
+    "url": "./static/css/main.103f1522.chunk.css"
+  },
+  {
+    "revision": "39a5021ea8a0dbc0dd56",
+    "url": "./static/js/2.936f7a40.chunk.js"
+  },
+  {
+    "revision": "86523f6a828f7dc132bf84d0d639b737",
+    "url": "./static/js/2.936f7a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8cca3de527486acb21",
+    "url": "./static/js/main.1f42b18d.chunk.js"
+  },
+  {
+    "revision": "8cf47f79272224c7e123",
+    "url": "./static/js/runtime-main.04a5655a.js"
+  }
+]);
