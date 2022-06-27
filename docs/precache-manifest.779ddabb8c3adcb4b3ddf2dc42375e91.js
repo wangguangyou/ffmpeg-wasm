c2f19819394b739cd8d88b936e1e94e6473353a5
wangguangyou <15968848474@100fen.cn>
2022-06-27 17:48:24 +0800

    docs

---
+++ docs/precache-manifest.779ddabb8c3adcb4b3ddf2dc42375e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec0430b2d0d62e12bc19e01fb29fe56",
+    "url": "/ffmpeg-wasm/index.html"
+  },
+  {
+    "revision": "6966b5801253e5daf974",
+    "url": "/ffmpeg-wasm/static/css/main.6810fe62.chunk.css"
+  },
+  {
+    "revision": "a0e2b3a10bc018a665cd",
+    "url": "/ffmpeg-wasm/static/js/2.fec74e75.chunk.js"
+  },
+  {
+    "revision": "86523f6a828f7dc132bf84d0d639b737",
+    "url": "/ffmpeg-wasm/static/js/2.fec74e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6966b5801253e5daf974",
+    "url": "/ffmpeg-wasm/static/js/main.238f3597.chunk.js"
+  },
+  {
+    "revision": "b9f58afb4aeb4db29776",
+    "url": "/ffmpeg-wasm/static/js/runtime-main.96a717cb.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.49fce9ae4af2bdfea49de6ecf26d3d61.js"
+  "/ffmpeg-wasm/precache-manifest.779ddabb8c3adcb4b3ddf2dc42375e91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ffmpeg-wasm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
